0326427f08c081ccac1f10f9a8dc6680e9492c01
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-06-17 16:37:59 +0000

    Finished with hero section

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PageLink from "@/components/ui/PageLink/PageLink";
 import styles from "./HomePage.module.css";
+import Image from "next/image";
+import heroImage from "public/photographer_04.jpg";
 
 const HomePage = () => {
   return (
@@ -16,7 +18,28 @@ const HomePage = () => {
           </PageLink>
         </div>
       </header>
-      <main className={`${styles.wrapper} body-main`}></main>
+      <main className={`${styles.wrapper} body-main`}>
+        <section className={`${styles.section} ${styles["hero-section"]}`}>
+          <div className={styles["hero-text"]}>
+            <h1 className="heading--primary">Access pictures from events</h1>
+            <p>
+              Easily access images from your events and photoshoots without
+              stress and from the comfort of your home. Be in control.{" "}
+            </p>
+            <div>
+              <PageLink url={"#"} type={"background"}>
+                Get started
+              </PageLink>
+            </div>
+          </div>
+          <Image
+            priority
+            src={heroImage}
+            className={styles["hero-img"]}
+            alt={"photograper taking a picture of a client"}
+          />
+        </section>
+      </main>
     </div>
   );
 };
